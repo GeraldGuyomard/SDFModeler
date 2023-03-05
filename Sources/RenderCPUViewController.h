05ca7de7b9683c747e3433845203e8cd2209bbb4
Gerald Guyomard <gerald@daysofwonder.com>
2023-03-04 22:05:59 -0800

    Added RenderCPUViewController

---
+++ Sources/RenderCPUViewController.h
@@ -0,0 +1,13 @@
+//
+//  RenderCPUViewController.m
+//  SDFModeler
+//
+//  Created by Gérald Guyomard on 3/4/23.
+//
+
+#import <Cocoa/Cocoa.h>
+
+// Our macOS view controller.
+@interface RenderCPUViewController : NSViewController
+
+@end
---
+++ Sources/RenderCPUViewController.mm
@@ -0,0 +1,18 @@
+//
+//  RenderCPUViewController.m
+//  SDFModeler
+//
+//  Created by Gérald Guyomard on 3/4/23.
+//
+
+#import "RenderCPUViewController.h"
+
+@implementation RenderCPUViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+}
+
+@end
