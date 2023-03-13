be56397c989d512427ad6c51b97607aef048d907
Gerald Guyomard <gerald@daysofwonder.com>
2023-03-12 18:36:14 -0700

    other normal computation algo

--- Sources/SDFGeometry/SDFGeometry.h
+++ Sources/SDFGeometry/SDFGeometry.h
@@ -25,6 +25,16 @@ private:
 template <typename TSDFGeometry>
 float3 computeNormal(TSDFGeometry geometry, float dist, float3 position)
 {
+#if 1
+    
+    constexpr float h = 0.0001; // replace by an appropriate value
+    const float2 k { h, -h };
+    return normalize(   k.xyy * geometry.computeDistance( position + k.xyy ) +
+                        k.yyx * geometry.computeDistance( position + k.yyx ) +
+                        k.yxy * geometry.computeDistance( position + k.yxy ) +
+                        k.xxx * geometry.computeDistance( position + k.xxx ) );
+    
+#else
     constexpr float delta = 0.01f;
     float2 eps { delta, 0.f };
     
@@ -33,5 +43,7 @@ float3 computeNormal(TSDFGeometry geometry, float dist, float3 position)
         geometry.computeDistance(position + eps.yxy) - dist,
         geometry.computeDistance(position + eps.yyx) - dist
     });
+    
+#endif
 }
 
