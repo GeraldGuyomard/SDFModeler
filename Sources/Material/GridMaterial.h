0128799beceb70f08d989db1dd9a7e85cffae1f4
Gerald Guyomard <gerald@daysofwonder.com>
2023-04-02 14:02:20 -0700

    keep nearest sampling

--- Sources/Material/GridMaterial.h
+++ Sources/Material/GridMaterial.h
@@ -20,7 +20,16 @@ public:
     
     float4 computeAlbedo(Ray ray, float dist, float3 p) const
     {
-        return computeAlbedoNearest(ray, dist);
+        const float4 primaryColor = computeAlbedoNearest(ray, dist);
+        return primaryColor;
+        /*
+        const float4 c0 = computeAlbedoNearest(ray.computeNeighbourRay({-1.f, 0.f}), dist);
+        const float4 c1 = computeAlbedoNearest(ray.computeNeighbourRay({0.f, 1.f}), dist);
+        const float4 c2 = computeAlbedoNearest(ray.computeNeighbourRay({1.f, 1.f}), dist);
+        const float4 c3 = computeAlbedoNearest(ray.computeNeighbourRay({-1.f, 1.f}), dist);
+        
+        constexpr float kPrimaryWeight = 1.f;
+        return ((primaryColor * kPrimaryWeight) + c0 + c1 + c2 + c3) / (4 + kPrimaryWeight);*/
     }
     
 private:
@@ -38,8 +47,7 @@ private:
         
         xy = max(-s, 0.f) + (s * xy);
 
-        xy = smoothstep(0.95, 0.98, xy);
-        //xy = step(0.98, xy);
+        xy = step(0.98, xy);
         
         float pixelVisible = min(1.f, xy.x + xy.y);
         float4 c = _color * pixelVisible;
