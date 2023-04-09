5ff96766c80c6b5721bb28f08046d5b0872aba06
Gerald Guyomard <gerald@daysofwonder.com>
2023-04-09 10:49:17 -0700

    removed dead code

--- Sources/SDFResult.h
+++ Sources/SDFResult.h
@@ -30,43 +30,5 @@ struct SDFResult final
     SDFResult(float distance, float4 color)
     : distance(distance), color(color)
     {}
-    
-    bool hit() const
-    {
-        return (distance >= 0.f) && (distance <= kDistanceEpsilon);
-    }
-    
-    bool isColorValid() const
-    {
-        return color.a != 0.f;
-    }
-    
-    bool isValid() const
-    {
-        return isColorValid() && hit();
-    }
 };
 
-template <typename TShader, typename TPrimitive>
-SDFResult computeSDF(float3 p, Ray ray, TShader shader, TPrimitive primitive)
-{
-    const float d = primitive.computeDistance(p);
-    
-    if (d > kDistanceEpsilon)
-    {
-        return { d, 0.f };
-    }
-    
-    const float4 c = shader.computeShade(primitive, ray, d, p);
-    
-    return { d, c };
-}
-
-template <typename TShader, typename TFirstPrimitive, typename... TPrimitives>
-SDFResult computeSDF(float3 p, Ray ray, TShader shader, TFirstPrimitive firstPrimitive, TPrimitives... primitives)
-{
-    SDFResult r1 = computeSDF(p, ray, shader, firstPrimitive);
-    SDFResult r2 = computeSDF(p, ray, shader, primitives...);
-    
-    return (r1.distance <= r2.distance) ? r1 : r2;
-}
