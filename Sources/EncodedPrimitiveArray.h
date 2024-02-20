6c913141591c2f772e56e68b128f68db38912a72
Gerald Guyomard <guyomard@adobe.com>
2024-02-20 14:33:47 -0800

    cleanup

--- Sources/EncodedPrimitiveArray.h
+++ Sources/EncodedPrimitiveArray.h
@@ -15,18 +15,6 @@ constexpr static CONSTANT size_t kNbObjectsMax = 64;
 using TPrimitiveOffset = int16_t;
 constexpr static CONSTANT TPrimitiveOffset kInvalidPrimitiveOffset = -1;
 
-INLINE float computeDistance(float3 pt, CONSTANT EncodedPrimitive* prim)
-{
-    DistanceEvaluator distanceEvaluator { pt };
-    
-    float2 distances { 1e7f, 1e7f };
-    
-    const float dist = evaluatePrimitive<DistanceEvaluator, float>(distanceEvaluator, prim);
-    distances[prim->operation] = min(distances[prim->operation], dist);
-    
-    return max(distances.x, -distances.y);
-}
-
 using TDrawCommandIndex = int16_t;
 struct DrawCommand final
 {
